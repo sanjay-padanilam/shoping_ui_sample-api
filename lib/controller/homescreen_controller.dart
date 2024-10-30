6f229cdb8d5692236f885c7a48eacb1d4dc77d69
sanjay-padanilam <sanjaysaji193@gmail.com>
2024-10-30 09:42:00 +0530

    commited

--- lib/controller/homescreen_controller.dart
+++ lib/controller/homescreen_controller.dart
@@ -1,17 +1,56 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:shoping_ui_sample/model/homescreen_moderl.dart';
 
 class HomescreenController with ChangeNotifier {
   Homescreenmodel? modelobj;
+  List cataorylist = [];
+  bool isloading = false;
 
-  Future<void> getproducts() async {
-    final url = Uri.parse("https://fakestoreapi.com/products");
+  // Future<void> getproducts() async {
+  //   final url = Uri.parse("https://fakestoreapi.com/products");
+  //   try {
+  //     var responce = await http.get(url);
+  //     if (responce.statusCode == 200) {
+  //       modelobj = homescreenmodelFromJson(responce.body);
+  //     }
+  //   } catch (e) {
+  //     print(e);
+  //   }
+  // }
+
+  Future<void> getcatagories() async {
+    isloading = true;
+    notifyListeners();
+    final catagoryUrl =
+        Uri.parse("https://fakestoreapi.com/products/categories");
     try {
-      var responce = await http.get(url);
+      var responce = await http.get(catagoryUrl);
       if (responce.statusCode == 200) {
-        modelobj = homescreenmodelFromJson(responce.body);
+        var convertedjson = jsonDecode(responce.body);
+        cataorylist = convertedjson;
+        cataorylist.insert(0, "All");
       }
-    } catch (e) {}
+    } catch (e) {
+      print(e);
+    }
+
+    notifyListeners();
+    isloading = false;
   }
+
+  oncatagoryselection() {
+    List screens = [
+      Container(color: Colors.red),
+      Container(color: Colors.blue),
+      Container(color: Colors.green),
+      Container(color: Colors.yellow),
+      Container(color: Colors.orange),
+    ];
+  }
+
+  getallProducts() {}
+  getproductBycatagory() {}
 }
